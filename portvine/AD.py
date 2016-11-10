0d7dad7c079b1debf3b659cef06eefe82ff9be0e
Sylwester Arabas <sarabas@chathamfinancial.eu>
2016-11-10 11:15:09 +0100

    first Python files (dlopen stuff using CFFI, one test using py.test)

---
+++ portvine/AD.py
@@ -0,0 +1,20 @@
+from portvine import *
+import numpy
+
+ffi.cdef("void ADtest(double* cdf, int* n, double* out);")
+
+
+def AD(cdf):
+    cdf = numpy.asarray(cdf, dtype=numpy.float64)
+
+    size = ffi.new("int*")
+    size[0] = ffi.cast("int", len(cdf))
+
+    ret = ffi.new("double*")
+
+    lib.ADtest(
+        ffi.cast("double*", cdf.ctypes.data),
+        size,
+        ret
+    )
+    return ret[0]
