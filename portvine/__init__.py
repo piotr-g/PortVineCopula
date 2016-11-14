11378f99fef2f06b25931cbf0d0fbfafa540dfe1
Sylwester Arabas <sarabas@chathamfinancial.eu>
2016-11-14 18:55:29 +0100

    trying to install R on Travis - continued

--- portvine/__init__.py
+++ portvine/__init__.py
@@ -7,6 +7,8 @@ if sys.platform == 'win32':
     os.environ['PATH'] += os.pathsep + 'C:\\Program Files\\R\\R-3.3.2\\bin\\x64'
     os.environ['PATH'] += os.pathsep + 'C:\\Program Files\\R\\R-3.3.2\\library\\VineCopula\\libs\\x64'
     lib = ffi.dlopen("VineCopula.dll")
+elif sys.platform == 'linux':
+    lib = ffi.dlopen("/usr/local/lib/R/site-library/VineCopula/libs/VineCopula.so")
 else:
     raise Exception("unsupported platform")
 
